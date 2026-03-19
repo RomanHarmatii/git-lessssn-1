a5900ef527b2c99ad365a07529dbde09d2de1ed2
RomanHarmatii <romaharmatii01@gmail.com>
2026-03-19 19:20:09 +0200

    hw

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
-console.log('Hello world')
+// let testData;
+// if (true) {
+//     testData = 1;
+//     console.log(testData);
+// }
+// if (true) {
+//     console.log(testData);
+// }
+// if (true) {
+//     console.log(testData);
+// }
+
+// const num = 0.1 + 0.2;
+// console.log(Math.floor(Math.random() + ) * 10);
+
+// const str = "The quick brown fox jumps over the lazy dog.";
+
+// console.log(str[str.length - 1]);
+
+// console.log(!!0)
+
+const cond1 = false;
+const cond2 = 1;
+
+if (!cond1) {
+  console.log("Hi");
+}
